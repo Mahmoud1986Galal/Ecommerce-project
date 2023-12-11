3470b59313d16642d009c53f162f9a978c3520cc
Mahmod Galal <mahmoud1986galal@gmail.com>
2023-12-11 04:17:51 +0200

    first commit

--- src/App.js
+++ src/App.js
@@ -2,9 +2,9 @@ import { HashRouter, Route, Routes } from "react-router-dom";
 import HomePage from "./pages/Home/HomePage";
 import NavBarLogin from "./component/Utility/NavBarLogin";
 import Footer from "./component/Utility/Footer";
-import LoginPage from "./pages/Auth/LoginPage";
 import RegisterPage from "./pages/Auth/RegisterPage";
 import AllCategoryPage from "./pages/Category/AllCategoryPage";
+import LoginPage from "./pages/Auth/LoginPage";
 import AllBrandPage from "./pages/Brands/AllBrandPage";
 import ShopProductPage from "./pages/Products/ShopProductPage";
 
@@ -12,16 +12,16 @@ function App() {
   return (
     <div className="font" >
       <HashRouter basename="ecommerce-project/">
+        <NavBarLogin />
         <Routes>
-          <Route path="/" element={<NavBarLogin />} />
           <Route index element={<HomePage />} />
           <Route path="/login" element={<LoginPage />} />
           <Route path="/register" element={<RegisterPage />} />
           <Route path="/allcategory" element={<AllCategoryPage />} />
           <Route path="/allbrand" element={<AllBrandPage />} />
           <Route path="/allproducts" element={<ShopProductPage />} />
-          <Route path="/" element={<Footer />} />
         </Routes>
+        <Footer />
       </HashRouter>
     </div>
   );
